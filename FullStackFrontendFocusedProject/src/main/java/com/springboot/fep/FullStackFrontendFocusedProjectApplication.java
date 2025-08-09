9d418c2247fed6c6f8205dcb80551ffac22ac768
HudayfaOmar23 <hudayfa.omar23@gmail.com>
2025-08-09 23:11:54 +0100

    Setup
    Setting up the backend and building the outline for my project

---
+++ FullStackFrontendFocusedProject/src/main/java/com/springboot/fep/FullStackFrontendFocusedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.fep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackFrontendFocusedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackFrontendFocusedProjectApplication.class, args);
+	}
+
+}
